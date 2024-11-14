0515e01c914f4474a44a5fc75ed94c224145ff20
ghtjr410 <ghtjr410@gmail.com>
2024-11-14 14:53:28 +0900

    view-count - 초기 프로젝트 생성   - jpa, spring web, mysql driver, lombok 의존성 주입

---
+++ viewcount-service/src/main/java/com/miniblog/viewcount/ViewcountServiceApplication.java
@@ -0,0 +1,13 @@
+package com.miniblog.viewcount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViewcountServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViewcountServiceApplication.class, args);
+	}
+
+}
---
+++ viewcount-service/src/test/java/com/miniblog/viewcount/ViewcountServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miniblog.viewcount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViewcountServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
