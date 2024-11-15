2bfe5987741cdbaeac2fdfd114922b5fbaf8284a
ghtjr410 <ghtjr410@gmail.com>
2024-11-15 15:16:23 +0900

    comment-service - JacksonConfi.java   - ObjectMapper 빈(bean)을 생성하여 JSON 직렬화 및 역직렬화에 사용 - KafkaProducerConfig.java   - Kafka 프로듀서 설정 클래스 추가   - Kafka 서버 부트스트랩 주소(bootstrap-servers)와 스키마 레지스트리 URL(schema.registry.url)을 외부에서 주입받음   - ProducerFactory와 KafkaTemplate을 구성하여 Kafka 메시지 전송을 가능하게 함   - Key/Value 시리얼라이저로 StringSerializer와 KafkaAvroSerializer를 사용   - 재시도 및 배달 시간 제한, ACK 설정, 메시지 순서 보장 등의 설정 포함 - EventProducer.java   - 이벤트 전송을 위한 프로듀서 클래스 추가   - @Retryable을 사용하여 예외 발생 시 최대 5회까지 재시도하며, 지수 백오프(backoff)를 적용하여 재시도 간의 지연 시간 증가   - publishEvent 메서드를 통해 특정 토픽에 이벤트 전송 기능 구현   - @Recover 어노테이션을 통해 재시도가 모두 실패한 경우 오류 로그를 남기고 Outbox 이벤트 상태를 실패로 업데이트

---
+++ comment-service/src/main/java/com/miniblog/comment/producer/EventProducer.java
@@ -0,0 +1,70 @@
+package com.miniblog.comment.producer;
+
+import com.miniblog.comment.model.OutboxEvent;
+import com.miniblog.comment.repository.OutboxEventRepository;
+import com.miniblog.comment.util.SagaStatus;
+import io.micrometer.tracing.Span;
+import io.micrometer.tracing.Tracer;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.avro.specific.SpecificRecordBase;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.retry.RetryContext;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Recover;
+import org.springframework.retry.annotation.Retryable;
+import org.springframework.retry.support.RetrySynchronizationManager;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.ExecutionException;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class EventProducer {
+    private final KafkaTemplate<String, SpecificRecordBase> kafkaTemplate;
+    private final OutboxEventRepository outboxEventRepository;
+    private final Tracer tracer;
+
+    @Retryable(
+            value = {Exception.class},
+            maxAttempts = 5,
+            backoff = @Backoff(delay = 1000, multiplier = 2)
+    )
+    public <T extends SpecificRecordBase> void publishEvent(String topicName, String key, T event, OutboxEvent outboxEvent, Span span) {
+        RetryContext context = RetrySynchronizationManager.getContext();
+        int retryCount = context != null ? context.getRetryCount() : 0;
+        log.info("이벤트 발행 시도 중, key: {}, attempt number: {}", key, retryCount + 1);
+
+        try (Tracer.SpanInScope ws = tracer.withSpan(span)) {
+            try {
+                log.info("topicName = {}, key = {}, event = {}", topicName, key, event);
+                kafkaTemplate.send(topicName, key, event).get(); // 동기화하여 예외처리
+                handleSuccess(outboxEvent);
+            } catch (InterruptedException | ExecutionException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    @Recover
+    public void recover(Exception ex, String topicName, String key, SpecificRecordBase event, OutboxEvent outboxEvent, Span span) {
+        try (Tracer.SpanInScope ws = tracer.withSpan(span))  {
+            log.error("이벤트 발행 실패: {}", ex.getMessage(), ex);
+            handleFailure(outboxEvent, ex);
+        }
+    }
+
+    private void handleSuccess(OutboxEvent outboxEvent) {
+        outboxEvent.setProcessed(true);
+        outboxEvent.setSagaStatus(SagaStatus.COMPLETED);
+        outboxEventRepository.save(outboxEvent);
+        log.info("Event Successfully processed and marked as COMPLETED.");
+    }
+
+    private void handleFailure(OutboxEvent outboxEvent, Exception ex) {
+        outboxEvent.setSagaStatus(SagaStatus.FAILED);
+        outboxEventRepository.save(outboxEvent);
+        log.error("Failed to send message after retries: {}", ex.getMessage());
+    }
+}
