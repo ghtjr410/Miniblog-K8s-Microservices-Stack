c007571b4c88977f80c6421ccdf254492231ddc4
ghtjr410 <ghtjr410@gmail.com>
2024-11-29 19:02:07 +0900

    post-service - 리팩토링 예정에 따라 변경될 클래스 삭제 - application.properties   - Kafka Consumer 설정 추가   - Consumer Group 설정 추가   - Consume 할 토픽 추가

--- post-service/src/main/java/com/miniblog/post/repository/PostRepository.java
+++
@@ -1,9 +0,0 @@
-package com.miniblog.post.repository;
-
-import com.miniblog.post.model.Post;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PostRepository extends JpaRepository<Post, Long> {
-}
--- post-service/src/main/java/com/miniblog/post/util/EventType.java
+++
@@ -1,7 +0,0 @@
-package com.miniblog.post.util;
-
-public enum EventType {
-    POST_CREATE,
-    POST_UPDATE,
-    POST_DELETE
-}
