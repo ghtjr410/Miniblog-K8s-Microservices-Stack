e0c815565a4cb3504a13b36f659122d739754986
ghtjr410 <ghtjr410@gmail.com>
2024-12-01 01:03:12 +0900

    comment-service - ConsumerPropertiesConfig.java   - Kafka 소비자 설정을 위한 consumerConfigs 메서드 구현.   - BOOTSTRAP_SERVERS_CONFIG, ENABLE_AUTO_COMMIT_CONFIG, AUTO_OFFSET_RESET_CONFIG 등 다양한 설정 추가.   - SpecificAvroDeserializer 설정을 통해 Avro 이벤트 처리 가능. - KafkaErrorHandlerConfig.java   - ExponentialBackOff를 사용한 재시도 로직 구현.   - handleFailure 메서드를 통해 에러 로그와 이벤트 정보 처리. - KafkaConsumerFactoryConfig.java   - ConsumerFactory와 ConcurrentKafkaListenerContainerFactory Bean 생성.   - ErrorHandler 및 Observation 활성화 설정. - KafkaProducerConfig.java   - Kafka ProducerFactory와 KafkaTemplate 설정.   - KEY_SERIALIZER, VALUE_SERIALIZER, SCHEMA_REGISTRY_URL 등 설정 추가.   - 무한 재시도 및 Idempotence 활성화를 통해 신뢰성 있는 메시지 전송 지원.

---
+++ comment-service/src/main/java/com/miniblog/comment/config/consume/ConsumerPropertiesConfig.java
@@ -0,0 +1,39 @@
+package com.miniblog.comment.config.consume;
+
+import io.confluent.kafka.serializers.AbstractKafkaSchemaSerDeConfig;
+import io.confluent.kafka.serializers.KafkaAvroDeserializer;
+import io.confluent.kafka.serializers.KafkaAvroDeserializerConfig;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.support.serializer.ErrorHandlingDeserializer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class ConsumerPropertiesConfig { // Kafka 소비자 설정을 제공
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootstrapServers;
+
+    @Value("${spring.kafka.properties.schema.registry.url}")
+    private String schemaRegistryUrl;
+
+    @Bean
+    protected Map<String, Object> consumerConfigs() {
+        Map<String, Object> configProps = new HashMap<>();
+        configProps.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        configProps.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false);
+        configProps.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        configProps.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, ErrorHandlingDeserializer.class);
+        configProps.put(ErrorHandlingDeserializer.KEY_DESERIALIZER_CLASS, StringDeserializer.class);
+        configProps.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, ErrorHandlingDeserializer.class);
+        configProps.put(ErrorHandlingDeserializer.VALUE_DESERIALIZER_CLASS, KafkaAvroDeserializer.class);
+        configProps.put(AbstractKafkaSchemaSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, schemaRegistryUrl);
+        configProps.put(KafkaAvroDeserializerConfig.SPECIFIC_AVRO_READER_CONFIG, true);
+
+        return configProps;
+    }
+}
---
+++ comment-service/src/main/java/com/miniblog/comment/config/consume/KafkaConsumerFactoryConfig.java
@@ -0,0 +1,34 @@
+package com.miniblog.comment.config.consume;
+
+import lombok.RequiredArgsConstructor;
+import org.apache.avro.specific.SpecificRecordBase;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.listener.ContainerProperties;
+
+@EnableKafka
+@Configuration
+@RequiredArgsConstructor
+public class KafkaConsumerFactoryConfig {
+    private final ConsumerPropertiesConfig consumerPropertiesConfig;
+    private final KafkaErrorHandlerConfig kafkaErrorHandlerConfig;
+
+    @Bean
+    public ConsumerFactory<String, SpecificRecordBase> consumerFactory() {
+        return new DefaultKafkaConsumerFactory<>(consumerPropertiesConfig.consumerConfigs());
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, SpecificRecordBase> kafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, SpecificRecordBase> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory());
+        factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.MANUAL);
+        factory.setCommonErrorHandler(kafkaErrorHandlerConfig.kafkaErrorHandler());
+        factory.getContainerProperties().setObservationEnabled(true);
+        return factory;
+    }
+}
