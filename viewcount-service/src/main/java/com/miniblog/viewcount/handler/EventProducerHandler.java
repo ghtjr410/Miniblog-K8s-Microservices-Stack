215dfa434d9c5e413c1aef9887fb08e903469c5e
ghtjr410 <ghtjr410@gmail.com>
2024-12-04 22:21:34 +0900

    viewcount-service - 리팩토링할 클래스 삭제

--- viewcount-service/src/main/java/com/miniblog/viewcount/handler/EventProducerHandler.java
+++
@@ -1,9 +0,0 @@
-package com.miniblog.viewcount.handler;
-
-import com.miniblog.viewcount.model.OutboxEvent;
-import com.miniblog.viewcount.util.ProducedEventType;
-
-public interface EventProducerHandler {
-    void handleEvent(OutboxEvent outboxEvent) throws Exception;
-    ProducedEventType getEventType();
-}
