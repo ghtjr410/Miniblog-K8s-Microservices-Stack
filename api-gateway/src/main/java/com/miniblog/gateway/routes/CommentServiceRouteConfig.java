f3b20ac96230f1cda75144dfc78fc464bd3ca6eb
ghtjr410 <ghtjr410@gmail.com>
2024-12-15 23:14:48 +0900

    api-gateway - CommentServiceRouteConfig.java   - JwtHeaderFilter가 주입되어 토큰에서 sub 값을 추출하고, 이를 커스텀 헤더에 추가하도록 설정   - JWT에서 추출한 sub를 커스텀 헤더로 추가하는 필터를 GatewayRouterFunctions에 적용   - HeaderType.SUB를 기반으로 sub 값을 커스텀 헤더에 추가하도록 설정 이렇게 추가된 커스텀 헤더는 다운스트림 서비스에서 요청 사용자의 userUuid를 알 수 있게 됩니다.

--- api-gateway/src/main/java/com/miniblog/gateway/routes/CommentServiceRouteConfig.java
+++ api-gateway/src/main/java/com/miniblog/gateway/routes/CommentServiceRouteConfig.java
@@ -1,5 +1,8 @@
 package com.miniblog.gateway.routes;
 
+import com.miniblog.gateway.filter.JwtHeaderFilter;
+import com.miniblog.gateway.util.HeaderType;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.server.mvc.filter.CircuitBreakerFilterFunctions;
 import org.springframework.cloud.gateway.server.mvc.filter.FilterFunctions;
@@ -12,8 +15,11 @@ import org.springframework.web.servlet.function.RouterFunction;
 import org.springframework.web.servlet.function.ServerResponse;
 
 import java.net.URI;
+import java.util.EnumSet;
+import java.util.Set;
 
 @Configuration
+@RequiredArgsConstructor
 public class CommentServiceRouteConfig {
     @Value("${comment.service.url}")
     private String commentServiceUrl;
@@ -24,10 +30,15 @@ public class CommentServiceRouteConfig {
     @Value("${springdoc.swagger-ui.urls[3].url}")
     private String commentServiceSwaggerUrl;
 
+    private final JwtHeaderFilter jwtHeaderFilter;
+
     @Bean
     public RouterFunction<ServerResponse> commentServiceRoute() {
+        Set<HeaderType> headersToInclude = EnumSet.of(HeaderType.SUB);
+
         return GatewayRouterFunctions.route("comment_service")
                 .route(RequestPredicates.path(commentServicePathPattern), HandlerFunctions.http(commentServiceUrl))
+                .filter(jwtHeaderFilter.addJwtHeadersFilter(headersToInclude))
                 .filter(CircuitBreakerFilterFunctions.circuitBreaker("commentServiceCircuitBreaker", URI.create("forward:/fallbackRoute")))
                 .build();
     }
