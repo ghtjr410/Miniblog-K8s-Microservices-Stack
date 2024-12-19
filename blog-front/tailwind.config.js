acfb410afcd6b51a7cc0734d3f7fc0e979049a13
ghtjr410 <ghtjr410@gmail.com>
2024-12-19 23:09:14 +0900

    blog-front - tailwind.config.js   - screens에 다양한 최대 너비 브레이크포인트 추가   - maxWidth에 사용자 정의 값 추가 - index.css   - 텍스트 깜빡임 애니메이션 추가     - 0%와 100%: 텍스트 색상 black     - 50%: 텍스트 색상 gray   - 호버 효과 클래스 추가     - 색상 변경을 부드럽게 적용하기 위해 transition: color 0.5s ease-in-out; 추가   - 호버 시 깜빡임 애니메이션 적용     - animation: text-blink 1s ease-in-out infinite; 적용

--- blog-front/tailwind.config.js
+++ blog-front/tailwind.config.js
@@ -4,10 +4,25 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    screens: {
+      '2xl': { 'max': '1920px' }, // 1919px 이하
+      'xl': { 'max': '1440px' },  // 1440px 이하
+      'lg': { 'max': '1200px' },
+      'md': { 'max': '1056px' },  // 1056px 이하
+      'sm': { 'max': '970px'}, //970px 이하
+      'xs': { 'max': '768px'},
+      '2xs': { 'max': '425px'},
+    },
     extend: {
       boxShadow: {
         'custom-default': 'rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 2px 6px 2px',
       },
+      maxWidth: {
+        '1728': '1728px', // 기본 max-width
+        '1376': '1376px', // 2xl 일때
+        '1024': '1024px', // xl 일때
+        '768': '768px',
+      }
     },
   },
   plugins: [
