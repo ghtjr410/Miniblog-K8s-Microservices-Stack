f73280bec654dbac7d7038ac03eea1ef3e3be1ed
ghtjr410 <ghtjr410@gmail.com>
2024-12-24 23:24:02 +0900

    profile-service - ProfileRequestDTO.java   - title 필드에 최대 길이 10자의 제한 추가 및 유효성 검사 메시지 정의.   - intro 필드에 최대 길이 50자의 제한 추가 및 유효성 검사 메시지 정의.

--- profile-service/src/main/java/com/miniblog/profile/dto/request/ProfileRequestDTO.java
+++ profile-service/src/main/java/com/miniblog/profile/dto/request/ProfileRequestDTO.java
@@ -1,12 +1,15 @@
 package com.miniblog.profile.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 public record ProfileRequestDTO(
         @NotBlank(message = "Nickname is required.")
         String nickname,
         @NotBlank(message = "Email is required.")
         String email,
+        @Size(max = 10, message = "Title can be up to 10 characters long.")
         String title,
+        @Size(max = 50, message = "Intro can be up to 50 characters long.")
         String intro) {
 }
