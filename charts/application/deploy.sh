cd31dd0a093d6e52b70767e0ab7e88b6d0c56c53
ghtjr410 <ghtjr410@gmail.com>
2025-02-25 08:32:15 +0900

    application - deploy.sh   - Helm을 사용하여 여러 서비스 배포를 위한 스크립트 추가.   - API Gateway, 백엔드 서비스, 프론트엔드 애플리케이션 포함.   - `helm install` 명령어를 사용하여 각 서비스를 개별적으로 설치하도록 구성.

---
+++ charts/application/deploy.sh
@@ -0,0 +1,12 @@
+helm install api-gateway ./api-gateway
+helm install account-service ./account-service
+helm install comment-service ./comment-service
+helm install image-service ./image-service
+helm install like-service ./like-service
+helm install post-service ./post-service
+helm install profile-service ./profile-service
+helm install viewcount-service ./viewcount-service
+helm install query-service ./query-service
+
+helm install blog-front ./blog-front
+helm install musical-front ./musical-front
